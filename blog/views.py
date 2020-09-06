5df92a4cd78e48eaf97453e42aebb48e309ca8d0
unknown <iromoon@gmail.com>
2020-09-07 02:16:28 +0900

    instantly stopped

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
 
-def index(request):
-    posts = Post.objects.all()
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-            'a_plus_b' : 1 + 3,
-        }
-    )
